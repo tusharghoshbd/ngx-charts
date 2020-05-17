66cc4a8094a74c6260f6bccecbe7f6849ac3cd37
Tushar <tusharghosh09006@gmail.com>
2020-05-17 10:10:54 +0600

    pie chart now is under shape
    it's now showing in the center and color

--- projects/ngx-charts-test/src/app/app.component.ts
+++ projects/ngx-charts-test/src/app/app.component.ts
@@ -10,6 +10,10 @@ export class AppComponent implements OnInit{
     categoriesp: any =[];
     seriesp: any=[];
 
+    optionsd: any={};
+    categoriesd: any =[];
+    seriesd: any=[];
+
     optionsl: any={};
     categoriesl: any =[];
     seriesl: any=[];
@@ -23,6 +27,45 @@ export class AppComponent implements OnInit{
     seriesh: any=[];
         
     ngOnInit() {
+        this.optionsd={
+            title: 'Monthly Average Rainfall',
+            subtitle: 'Source: WorldClimate.com',
+            height: 400,
+            //width: 700,
+            xAxis: {
+                title: 'Month name',
+                labelRotation: 0,
+                labelAlign:'middle' // left, middle, right
+            },
+            yAxis: {
+                title: 'Rainfall (mm)'
+            },
+            plotOptions: {
+                outerRadius:120,
+                innerRadius:80
+            }
+        };
+        
+        setTimeout(() => { 
+            this.seriesd=[{
+                name: 'Chrome',
+                data: 61.41
+            }, {
+                name: 'IE',
+                data: 11.84
+            }, {
+                name: 'Firefox',
+                data: 10.84
+            }, {
+                name: 'Opera',
+                data: 15
+            }, {
+                name: 'Other',
+                data: 20
+            }]
+        }, 3000)
+        
+        // *********************************************
 
         this.optionsp={
             title: 'Monthly Average Rainfall',
@@ -38,28 +81,29 @@ export class AppComponent implements OnInit{
                 title: 'Rainfall (mm)'
             },
             plotOptions: {
+                outerRadius:120,
+                innerRadius:0
             }
         };
         
         setTimeout(() => { 
-            this.categoriesp=[2010,2011,2012,2013,2014,2015,2016,2017];
             this.seriesp=[{
-                name: 'Installation',
-                data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+                name: 'Chrome',
+                data: 61.41
             }, {
-                name: 'Manufacturing',
-                data: [24916, 24064, 29742, 29851, 32490, 49282, 38121, 40434]
+                name: 'IE',
+                data: 11.84
             }, {
-                name: 'Sales & Distribution',
-                data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+                name: 'Firefox',
+                data: 10.84
             }, {
-                name: 'Project Development',
-                data: [19771, 16005, 7988, 12169, 15112, 22452, 34400, 34227]
+                name: 'Opera',
+                data: 15
             }, {
                 name: 'Other',
-                data: [12908, 5948, 8105, 11248, 8989, 61816, 18274, 18111]
+                data: 20
             }]
-        },500)
+        },0)
 /** **************************************************************************** */
         this.optionsl={
             title: 'Monthly Average Rainfall',
--- projects/ngx-charts-test/src/app/ngx-charts-pie/ngx-charts-pie.component.ts
+++ projects/ngx-charts-test/src/app/ngx-charts-pie/ngx-charts-pie.component.ts
@@ -13,7 +13,7 @@ import {
 
 import { scaleBand, scaleLinear } from "d3-scale";
 import { ColorHelper } from '../utils/color.helper';
-import { ClassGetter } from '@angular/compiler/src/output/output_ast';
+import { arc, pie } from 'd3-shape';
 
 @Component({
     selector: "ngx-charts-pie",
@@ -22,7 +22,7 @@ import { ClassGetter } from '@angular/compiler/src/output/output_ast';
     // changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None
 })
-    
+
 export class ngxChartsPieComponent implements OnChanges, OnInit {
 
     private customOptions={
@@ -41,8 +41,8 @@ export class ngxChartsPieComponent implements OnChanges, OnInit {
         yAxis: {
             title: '',
             width: 0,
-            height:0,
-            labelRotation:0
+            height: 0,
+            labelRotation: 0
         },
         plotBackground: {
             x: 0,
@@ -50,9 +50,9 @@ export class ngxChartsPieComponent implements OnChanges, OnInit {
             height: 0,
             width: 0
         },
-        plotOptions: { 
-            groupBarPadding :20,
-            innerBarPadding :3
+        plotOptions: {
+            outerRadius:80,
+            innerRadius:0
         },
         header: {
             height: 0,
@@ -93,7 +93,7 @@ export class ngxChartsPieComponent implements OnChanges, OnInit {
             plotOptions: {
                 ...this.customOptions.plotOptions,
                 ...plotOptions
-                
+
             },
             header: {
                 ...this.customOptions.header,
@@ -114,165 +114,110 @@ export class ngxChartsPieComponent implements OnChanges, OnInit {
     xScale: any;
     innerScale: any;
     yScale: any;
-    lines: any=[];
-    lineCircle: any = [];
-    groupName: any[] = [];
-    groupBarPaddingBK: any;
-    innerBarPaddingBK: any;
+    pies: any=[];
+    lineCircle: any=[];
+    groupName: any[]=[];
+    // groupBarPaddingBK: any;
+    // innerBarPaddingBK: any;
     colorScale: any;
+    calcArc: any;
+    pieGenerator: any;
+    translation: string="";
 
     constructor(
         private chartElement: ElementRef,
-        private  cdr: ChangeDetectorRef) { }
+        private cdr: ChangeDetectorRef) { }
 
     ngOnChanges(changes: SimpleChanges): void {
-        this.groupBarPaddingBK=this.options.plotOptions.groupBarPadding;
-        this.innerBarPaddingBK=this.options.plotOptions.innerBarPadding;
+        // this.groupBarPaddingBK=this.options.plotOptions.groupBarPadding;
+        // this.innerBarPaddingBK=this.options.plotOptions.innerBarPadding;
         setTimeout(() => this.update());
     }
 
     ngOnInit() {
         this.options.width=this.options.width;
         this.options.height=this.options.height;
-        
         this.update();
     }
 
     update(): void {
         // console.log("ttttttt",this.options)
         const hostElem=this.chartElement.nativeElement;
-        let dims=hostElem.parentNode!==null? hostElem.parentNode.getBoundingClientRect():{height:400, width:800};
-        
-        var style=hostElem.parentNode.currentStyle||window.getComputedStyle(hostElem.parentNode);
-       
-        this.options.height = !this.options.height? dims.height - this.strToNumber(style.paddingLeft) - this.strToNumber(style.paddingRight)  :this.options.height;
-        this.options.width = !this.options.width ? dims.width- this.strToNumber(style.paddingLeft) - this.strToNumber(style.paddingRight)   : dims.width- this.strToNumber(style.paddingLeft) - this.strToNumber(style.paddingRight);
-        
-        this.calPlotBackground()
+        let dims=hostElem.parentNode!==null? hostElem.parentNode.getBoundingClientRect():{ height: 400, width: 800 };
 
-        let countFlag=false;
-        this.options.plotOptions.groupBarPadding=this.groupBarPaddingBK;
-        this.options.plotOptions.innerBarPadding=this.innerBarPaddingBK;
-        do {
-            if (countFlag==true) {
-                this.options.plotOptions.groupBarPadding--;
-                this.options.plotOptions.innerBarPadding = 2;
-            }
-            if (this.options.barType=='vertical') {
-                this.xScale=this.getXScale();
-            }
-            else { 
-                this.yScale=this.getXScale();
-            }
-            this.innerScale=this.getInnerScale();
-            countFlag=true;
-             
-        } while (this.innerScale.bandwidth()<2);
+        var style=hostElem.parentNode.currentStyle||window.getComputedStyle(hostElem.parentNode);
+
+        this.options.height=!this.options.height? dims.height-this.strToNumber(style.paddingLeft)-this.strToNumber(style.paddingRight):this.options.height;
+        this.options.width=!this.options.width? dims.width-this.strToNumber(style.paddingLeft)-this.strToNumber(style.paddingRight):dims.width-this.strToNumber(style.paddingLeft)-this.strToNumber(style.paddingRight);
+
+        this.calPlotBackground();
+        const xOffset = this.options.width / 2;
+        const yOffset = this.options.header.height + (this.options.plotBackground.height / 2);
+        this.translation = `translate(${xOffset}, ${yOffset})`;
+        this.calcArc=this.calculateArc();
+        this.pieGenerator=this.pieGeneratorFunc();
+
+        // console.log(this.pieGenerator)
+
+        // let countFlag=false;
+        // this.options.plotOptions.groupBarPadding=this.groupBarPaddingBK;
+        // this.options.plotOptions.innerBarPadding=this.innerBarPaddingBK;
+        // do {
+        //     if (countFlag==true) {
+        //         this.options.plotOptions.groupBarPadding--;
+        //         this.options.plotOptions.innerBarPadding = 2;
+        //     }
+        //     if (this.options.barType=='vertical') {
+        //         this.xScale=this.getXScale();
+        //     }
+        //     else { 
+        //         this.yScale=this.getXScale();
+        //     }
+        //     this.innerScale=this.getInnerScale();
+        //     countFlag=true;
+        // } while (this.innerScale.bandwidth()<2);
         // 
 
-        if (this.options.barType=='vertical') {
-            this.yScale=this.getYScale();
-        }
-        else { 
-            this.xScale=this.getYScale();
-        }
-        
+        // if (this.options.barType=='vertical') {
+        //     this.yScale=this.getYScale();
+        // }
+        // else { 
+        //     this.xScale=this.getYScale();
+        // }
+
         let colorHelper=new ColorHelper(this.options, this.series);
         this.colorScale=colorHelper.generateColorScale();
-        
-        
 
         setTimeout(() => {
             this.groupName=[];
-            this.series.map(item => { 
-                if (item.name)
+            this.series.map(item => {
+                if (item.name) {
                     this.groupName.push({
                         name: item.name,
                         color: this.colorScale(item.name)
                     });
+                }
             })
-            this.createLine();
+            this.createPie();
         });
         this.cdr.detectChanges();
     }
 
-    getXScale(): any {
-        
-        let spacing;
-        let range;
-        if (this.options.barType=='vertical') {
-            spacing=(this.categories.length/(this.options.plotBackground.width/this.options.plotOptions.groupBarPadding));
-            range= [0, this.options.plotBackground.width];
-        }
-        else { 
-            let length=this.options.height-this.options.header.height;
-            spacing=(this.categories.length/(this.options.plotBackground.height/this.options.plotOptions.groupBarPadding));
-            range=[0, this.options.plotBackground.height];
-        }
-        return scaleBand()
-            .range(range)
-            .paddingInner(spacing)
-            .paddingOuter(0.1)
-            .domain(this.categories);
-    }
-    getInnerScale(): any {
-
-        let groupDataArr=[];
-        for (let i=0; i<this.series.length; i++) { 
-            groupDataArr.push(this.series[i].name);
-        }
-
-        let spacing;
-        let range;
-        if (this.options.barType=='vertical') {
-            spacing=(this.series.length/(this.xScale.bandwidth()/this.options.plotOptions.innerBarPadding));
-            range=this.xScale.bandwidth();
-        }
-        else { 
-            spacing=(this.series.length/(this.yScale.bandwidth()/this.options.plotOptions.innerBarPadding));
-            range=this.yScale.bandwidth();
-        }
-
-        return scaleBand()
-            .range([0, range])
-            .paddingInner(spacing)
-            .domain(groupDataArr);
-    }
-    
-    getYScale(): any {
-        let uniqueValue: any=new Set();
-        this.series.map((item) => {
-            item.data.map((value) => {
-                uniqueValue.add(value);
-            });
-        });
-
-        let min=Math.min(...uniqueValue);
-        min=min>0? 0:min;
-        
-        let max=Math.max(0, ...uniqueValue);
-        max=max>0? max:0;
-        
-        let range = [];
-        if (this.options.barType=='vertical') {
-            let value=this.options.plotBackground.height;
-            // console.log("bar getYScale",value)
-            range=[value, 0];
-            // console.log("bar getYScale - ", range)
-        }
-        else { 
-            let value=this.options.plotBackground.width-30;
-            range=[0, value];
-        }
-
-        // console.log("bar getYScale --- ", range, min, max)
-
-        return scaleLinear()
-            .range(range)
-            .domain([min, max]);
-        //return this.scale.nice().ticks();
+    pieGeneratorFunc() { 
+       return  pie()
+        .value(d => d.data)
+        .sort(null)(this.series);
     }
 
+    calculateArc(): any {
+        let outerRadius=this.options.plotOptions.outerRadius;
+        let innerRadius=this.options.plotOptions.innerRadius;
+        let cornerRadius=0;
+        return arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius)
+            .cornerRadius(cornerRadius);
+    }
 
     calPlotBackground() {
         this.options={
@@ -281,42 +226,45 @@ export class ngxChartsPieComponent implements OnChanges, OnInit {
                 ...this.options.plotBackground,
                 x: 0,
                 y: 0,
-                height:this.options.height-this.options.xAxis.height-this.options.header.height-this.options.padding,
+                height: this.options.height-this.options.xAxis.height-this.options.header.height-this.options.padding,
                 width: this.options.width-this.options.yAxis.width-this.options.padding
             }
         }
         // console.log("calPlotBackground", JSON.stringify(this.options));
     }
-    createLine() {
+    createPie() {
         //console.log("this.innerScale.bandwidth() "+this.innerScale.bandwidth())
-        this.lines=[];
-        this.lineCircle=[];
-        for (let i=0; i<this.series.length; i++) { 
-            let line={points:"", color:""}
-            for (let j=0; j<this.categories.length; j++) { 
-                let x=this.xScale(this.categories[j])+(this.xScale.bandwidth()/2)+this.options.yAxis.width;
-                let y = this.yScale(this.series[i].data[j])+this.options.header.height
-                line.points+=(x +","+y+" ");
-                line.color=this.colorScale(this.series[i].name);
-                this.lineCircle.push({
-                    x,
-                    y,
-                    color: this.colorScale(this.series[i].name),
-                    value: this.categories[j],  //jan,feb
-                    data: this.series[i].data[j], //101,202
-                    group: this.series[i].name
-                });
-            }
-            this.lines.push(line);
+        this.pies=[];
+        for (let i=0; i<this.pieGenerator.length; i++) {
+            this.pies.push({
+                path: this.calcArc.startAngle(this.pieGenerator[i].startAngle).endAngle(this.pieGenerator[i].endAngle)(),
+                color: this.colorScale(this.pieGenerator[i].data.name),
+                data:this.pieGenerator[i].data
+            });
+
+            // for (let j=0; j<this.categories.length; j++) {
+            //     let x=this.xScale(this.categories[j])+(this.xScale.bandwidth()/2)+this.options.yAxis.width;
+            //     let y=this.yScale(this.series[i].data[j])+this.options.header.height
+            //     line.points+=(x+","+y+" ");
+            //     line.color=this.colorScale(this.series[i].name);
+            //     this.lineCircle.push({
+            //         x,
+            //         y,
+            //         color: this.colorScale(this.series[i].name),
+            //         value: this.categories[j],  //jan,feb
+            //         data: this.series[i].data[j], //101,202
+            //         group: this.series[i].name
+            //     });
+            // }
         }
 
-        // console.log(this.lineCircle);
-       
-       
-        
+        console.log(this.pies);
+
+
+
     }
 
-    yAxisWidthChange({ yAxisWidth,yAxisHeight }) {
+    yAxisWidthChange({ yAxisWidth, yAxisHeight }) {
         //console.log("yAxisWidth "+yAxisWidth)
         this.options={
             ...this.options,
@@ -353,11 +301,11 @@ export class ngxChartsPieComponent implements OnChanges, OnInit {
     }
 
 
-    toolTipPlaccement(data) { 
+    toolTipPlaccement(data) {
         if (this.options.barType=='vertical') {
             return data>0? 'top':'bottom'
         }
-        else { 
+        else {
             return data>0? 'right':'left'
         }
     }
@@ -367,12 +315,12 @@ export class ngxChartsPieComponent implements OnChanges, OnInit {
         setTimeout(() => this.update());
     }
 
-   
-    
-    
 
 
-    private strToNumber(str) { 
+
+
+
+    private strToNumber(str) {
         let numberPattern=/\d+/g;
         let num=str.match(numberPattern).join('')
         return parseFloat(num);
